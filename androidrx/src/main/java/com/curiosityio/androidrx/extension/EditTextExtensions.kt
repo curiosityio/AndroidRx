60155ab864a87645b059a9b3139f6e854912d1e5
Levi Bostian <levi@curiosityio.com>
2017-01-17 09:48:34 -0600

    Verify email added to EditText extensions.

--- androidrx/src/main/java/com/curiosityio/androidrx/extension/EditTextExtensions.kt
+++ androidrx/src/main/java/com/curiosityio/androidrx/extension/EditTextExtensions.kt
@@ -1,14 +1,27 @@
 package com.curiosityio.androidrx.extension
 
 import android.widget.EditText
+import com.curiosityio.androidrx.R
 import rx.Completable
 
 fun EditText.verifyNotEmpty(errorMessage: String): Completable {
     return Completable.create { subscriber ->
         if (text.toString().isBlank()) {
+            error = errorMessage
             subscriber.onError(RuntimeException(errorMessage))
         } else {
             subscriber.onCompleted()
+        }
+    }
+}
+
+fun EditText.verifyEmail(): Completable {
+    return Completable.create { subscriber ->
+        if (android.util.Patterns.EMAIL_ADDRESS.matcher(text).matches()) {
+            subscriber.onCompleted()
+        } else {
+            error = context.getString(R.string.enter_email_address)
+            subscriber.onError(RuntimeException(error!!.toString()))
         }
     }
 }
